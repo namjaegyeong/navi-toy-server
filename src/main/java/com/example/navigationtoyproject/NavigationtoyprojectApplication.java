208789a53a46cc3db34e063699446fe6a17100ab
Nam Jae Gyeong <57249939+namjaegyeong@users.noreply.github.com>
2025-01-08 11:45:02 +0900

    init commit

---
+++ src/main/java/com/example/navigationtoyproject/NavigationtoyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.navigationtoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigationtoyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationtoyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/navigationtoyproject/NavigationtoyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navigationtoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationtoyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
